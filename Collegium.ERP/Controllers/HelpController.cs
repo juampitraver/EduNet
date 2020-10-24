8a213f75847258eef706a07a62e42cef0b75c75d
Sergio Liuzzi <sergiozzi@gmail.com>
2020-10-24 18:01:07 -0300

    Adding views

---
+++ Collegium.ERP/Controllers/HelpController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP3.ERP.Controllers
+{
+    public class HelpController : Controller
+    {
+        public HelpController()
+        {
+        }
+
+        public IActionResult Index() => View();
+    }
+}
---
+++ Collegium.ERP/Controllers/TutorialController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP3.ERP.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public TutorialController()
+        {
+        }
+
+        public IActionResult Index() => View();
+    }
+}
