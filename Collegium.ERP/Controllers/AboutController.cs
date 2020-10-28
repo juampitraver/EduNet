d4e8e2b3a283742a83d89da7ba89a7b62e2515f5
Sergio Liuzzi <sergiozzi@gmail.com>
2020-10-28 10:15:36 -0300

    Adding and changes viewes

---
+++ Collegium.ERP/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP3.ERP.Controllers
+{
+    public class AboutController : Controller
+    {
+        public AboutController()
+        {
+        }
+
+        public IActionResult Index() => View();
+    }
+}
